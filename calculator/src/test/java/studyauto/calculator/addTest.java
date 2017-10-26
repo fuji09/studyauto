1ed752e7d5680652e4f9ff48e21c5a5243cc6e62
songna <carol87913@126.com>
2017-10-26 23:06:15 +0800

    initial checkin studyauto.

---
+++ calculator/src/test/java/studyauto/calculator/addTest.java
@@ -0,0 +1,24 @@
+package studyauto.calculator;
+
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class addTest {
+   
+	@DataProvider(name="add")
+    public Object[][] num(){
+		return new Object[][] {
+			{2,3,5},
+			{5,6,11},
+			{2,6,8},
+		};
+	}
+    
+	
+	@Test(dataProvider="add")
+	public void adTest(int input1,int input2,int expect) {
+		assertEquals(expect, new math().add(input1, input2));
+	}
+	
+}
